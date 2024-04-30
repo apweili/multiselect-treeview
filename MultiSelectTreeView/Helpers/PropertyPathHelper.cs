02511edbc09329602c96b692e508b338397c95bf
WeiLi <woshilw0416@icloud.com>
2024-04-30 15:24:27 +0800

    add file

---
+++ MultiSelectTreeView/Helpers/PropertyPathHelper.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+namespace System.Windows.Helpers
+{
+    public class PropertyPathHelper
+    {
+        public static object GetObjectByPropertyPath(object target, string path)
+        {
+            var p = target.GetType().GetProperty(path);
+            if (p == null)
+                throw new ArgumentException($"The property {path} could not be found.");
+
+            var result = p.GetValue(target, null);
+            return result;
+        }
+        
+        public static object GetDeepPropertyValue(object obj, string path)
+        {
+            if (string.IsNullOrWhiteSpace(path)) return obj;
+            while (true)
+            {
+                if (path.Contains("."))
+                {
+                    string[] split = path.Split('.');
+                    string remainingProperty = path.Substring(path.IndexOf('.') + 1);
+                    if (obj != null)
+                    {
+                        obj = obj.GetType().GetProperty(split[0])?.GetValue(obj, null);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                    path = remainingProperty;
+                    continue;
+                }
+
+                if (obj != null)
+                {
+                    return obj.GetType().GetProperty(path)?.GetValue(obj, null);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
