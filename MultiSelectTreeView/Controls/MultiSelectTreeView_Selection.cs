d103211aa80c85d127071c229901116c1f29bf02
WeiLi <woshilw0416@icloud.com>
2024-05-10 10:46:06 +0800

    delete unused reference

--- MultiSelectTreeView/Controls/MultiSelectTreeView_Selection.cs
+++ MultiSelectTreeView/Controls/MultiSelectTreeView_Selection.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Diagnostics.Contracts;
