71de47d865fd80b19bbd14d9e610777aa64cb43f
WeiLi <woshilw0416@icloud.com>
2024-05-22 16:46:22 +0800

    modify test

--- Demo/MainWindow.xaml.cs
+++ Demo/MainWindow.xaml.cs
@@ -173,7 +173,7 @@ namespace Demo
             // _nodeToAdd = node132;
             // rootNode.SelectedItems.Add(node1);
             // rootNode.SelectedItems.Add(node2);
-            TheSpecialTreeView.SelectionMode = TreeViewSelectionMode.MultiSelectEnabled;
+            TheSpecialTreeView.SelectionMode = TreeViewSelectionMode.SingleSelectOnly;
 
             // Loaded += (sender, args) =>
             // {
